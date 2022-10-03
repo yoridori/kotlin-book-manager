c881e09074fa69bf4b133f39389a2fa161a28f23
yoridori <midori.engineer@gmail.com>
2022-10-03 15:55:09 +0900

    初期コミット

---
+++ src/test/kotlin/com/yoridori/book/manager/BookManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yoridori.book.manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
